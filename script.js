ff513f2a883ada7fdb476a5ef2d46e510553cf6e
Aileen Bryand <aileenale09@gmail.com>
2022-07-02 00:26:30 -0500

    commit-it

---
+++ script.js
@@ -0,0 +1,17 @@
+//code quiz js
+
+
+//landing page with start button
+//when start button is clicked, remove welcome msg abd append question 1
+
+    //add event.litener
+        //activity 12
+//start timer
+//user answers questions-read chosen elements, data value, compare to answer key
+//iterate (for-loop to display questions
+//if correct, next question
+//if incorrect, sbstract 10 seconds from timer)
+//end of quiz
+    //add up user score
+    //display input
+    //second html and siplay username and score-- read object from local storage
